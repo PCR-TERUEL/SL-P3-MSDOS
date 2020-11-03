b31d1ca29d1523046fe71fb178004e78488319e8
Kifixo <danielhuici@hotmail.com>
2020-11-03 18:39:57 +0100

    insert data & order functionalities

--- src/controller/DOSWrapper.java
+++ src/controller/DOSWrapper.java
@@ -1,32 +1,94 @@
 package controller;
 
-import java.io.BufferedReader;
+import net.sourceforge.tess4j.Tesseract1;
+import net.sourceforge.tess4j.TesseractException;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.List;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
-import java.io.InputStreamReader;
+
 
 public class DOSWrapper {
+    private final Robot robot;
+    private Tesseract1 ocr;
+    private static final List<String> dataTypes = List.of("UTILIDAD", "ARCADE", "CONVERSACIONAL", "VIDEOAVENTRA",
+                                                          "SIMULADOR", "JUEGO DE MESA", "S. DEPORTIVO", "ESTRATEGIA");
+    private static final List<String> ORDER_TYPES = List.of("NOMBRE", "TIPO", "CINTA", "ANTIGUEDAD");
+    private static final String CMD_LAUNCH_DOSBOX = "cmd /c cd Database-MSDOS & start database.bat";
+    private static final int DOSBOX_EXEC_TIME = 5000;
+    private static final int ORDER_TIME = 30000;
+    private static final int ROBOT_DELAY = 100;
 
-    public DOSWrapper() throws IOException {
+    public DOSWrapper() throws IOException, InterruptedException, AWTException, TesseractException {
         launchDOSBox();
+        robot = new Robot();
+        initializeOCR();
+        getDatabaseInfo();
+
+        //insertData("Prueba", "SIMULADOR", "1");
+        //ordenar("NOMBRE");
     }
 
-    private void launchDOSBox() throws IOException {
-        printResults(Runtime.getRuntime().exec("cmd /c cd Database-MSDOS & start database.bat"));
+    private void initializeOCR() {
+        ocr = new Tesseract1();
+        ocr.setLanguage("spa");
+    }
 
+    private void launchDOSBox() throws IOException, InterruptedException {
+        Runtime.getRuntime().exec(CMD_LAUNCH_DOSBOX);
+        Thread.sleep(DOSBOX_EXEC_TIME);
     }
 
 
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException, InterruptedException, AWTException, TesseractException {
         new DOSWrapper();
     }
 
+    public void insertData(String name, String type, String cassette) {
+        robot.keyPress(KeyEvent.VK_1);
+        robot.keyRelease(KeyEvent.VK_1);
+        sendKeys(name);
+        sendKeys(type);
+        sendKeys(cassette);
+        sendEnterKey();
+        sendEnterKey();
+    }
 
-    public void printResults(Process process) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String line = "";
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
+    public void ordenar(String orden) throws InterruptedException {
+        sendKeys("3");
+        sendKeys(String.valueOf(ORDER_TYPES.indexOf(orden) + 1));
+        Thread.sleep(ORDER_TIME);
+        sendEnterKey();
+    }
+
+    public void getDatabaseInfo() throws TesseractException {
+        BufferedImage capture = robot.createScreenCapture(new Rectangle(80, 100, 720, 500));
+        System.out.println(ocr.doOCR(capture));
+    }
+
+    private void sendKeys(String keys) {
+        for (char c : keys.toCharArray()) {
+            int keyCode = KeyEvent.getExtendedKeyCodeForChar(c);
+            if (KeyEvent.CHAR_UNDEFINED == keyCode) {
+                throw new RuntimeException("Key code not found for character " + c);
+            }
+            robot.keyPress(keyCode);
+            robot.delay(ROBOT_DELAY);
+            robot.keyRelease(keyCode);
+            robot.delay(ROBOT_DELAY);
         }
+        sendEnterKey();
+    }
+
+    private void sendEnterKey() {
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.delay(100);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+        robot.delay(100);
     }
 
 
